f7e3e6c88c2865e6c05fd0fb634bd378fd051b1e
Divya <divyamakkar000@gmail.com>
2023-08-02 19:48:15 -0400

    fishnet on campgrounds

--- campgroundsKNN.py
+++ campgroundsKNN.py
@@ -1,48 +1,95 @@
 import geopandas as gpd
 import matplotlib.pyplot as plt
+from shapely.geometry import Polygon
 
 california_data = gpd.read_file("./testData/CA_Counties_TIGER2016.geojson")
 campground_data = gpd.read_file("./testData/Campground Sites.geojson")
+wildfire_data = gpd.read_file("./testData/California_Fire_Perimeters_(all).geojson")
+
+print(wildfire_data.crs)
 
 # Check the CRS of both datasets
 print(california_data.crs)
 print(campground_data.crs)
 
-# Reproject the campground data to match the CRS of California counties data
+# Reproject the wildfire data to match the CRS of California counties data
 campground_data_reprojected = campground_data.to_crs(california_data.crs)
 
-# Perform a spatial join to associate campgrounds with California counties
+# Perform a spatial join to associate wildfires with California counties
 campgrounds_in_california = gpd.sjoin(campground_data_reprojected, california_data, op="within")
 
-# Create a fishnet/grid covering the extent of California
-minx, miny, maxx, maxy = california_data.total_bounds
-cell_size = 0.05  # Adjust the cell size as needed
-grid = gpd.GeoDataFrame.from_features(
-    [
-        {
-            'geometry': box(x, y, x+cell_size, y+cell_size),
-            'id': f"{x}_{y}"
-        }
-        for x in range(int(minx), int(maxx), int(cell_size*1000))
-        for y in range(int(miny), int(maxy), int(cell_size*1000))
-    ],
-    crs=california_data.crs
-)
+wildfire_data_reprojected = wildfire_data.to_crs(california_data.crs)
 
-# Clip the fishnet/grid to the boundary of California
-grid_clipped = gpd.clip(grid, california_data)
+# Calculate the bounding box for California counties
+bbox = california_data.total_bounds
+print(bbox)
 
-# Plotting the map
+print("fishnet stage 1 loading ...")
+# Create a fishnet grid with smaller cells covering the bounding box of California counties
+cell_size = 5000  # Cell size in meters (25 km = 25000 meters)
+rows = int((bbox[3] - bbox[1]) / cell_size)
+cols = int((bbox[2] - bbox[0]) / cell_size)
+
+# Generate polygons for each cell in the fishnet
+grid_polygons = []
+for x in range(cols):
+    for y in range(rows):
+        xmin = bbox[0] + x * cell_size
+        xmax = bbox[0] + (x + 1) * cell_size
+        ymin = bbox[1] + y * cell_size
+        ymax = bbox[1] + (y + 1) * cell_size
+
+        # Clip the fishnet cell to the extent of California
+        clipped_polygon = Polygon([(max(xmin, bbox[0]), max(ymin, bbox[1])),
+                                   (min(xmax, bbox[2]), max(ymin, bbox[1])),
+                                   (min(xmax, bbox[2]), min(ymax, bbox[3])),
+                                   (max(xmin, bbox[0]), min(ymax, bbox[3]))])
+        interscting_california = california_data[california_data.intersects(clipped_polygon)]
+        if not interscting_california.empty:
+            grid_polygons.append(clipped_polygon)
+
+print("checking fishnet ...")
+# Create a GeoDataFrame from the grid polygons
+fishnet = gpd.GeoDataFrame({'geometry': grid_polygons}, crs=california_data.crs)
+
+# Create a list to store the color for each fishnet cell
+fishnet_colors = []
+
+wildfire_coordinates = []
+
+# Iterate through the fishnet cells and check if there's any wildfire inside
+for cell_polygon in fishnet['geometry']:
+    # Check if any wildfire intersects with the fishnet cell
+    intersecting_campgrounds = campgrounds_in_california[campgrounds_in_california.intersects(cell_polygon)]
+    intersecting_wildfires = wildfires_in_california[wildfires_in_california.intersects(cell_polygon)]
+
+    if not intersecting_wildfires.empty:
+        # If there are wildfires inside the cell, color it red
+        wildfire_coordinates.append([cell_polygon.centroid_x, cell_polygon.centroid_y])
+
+    if not intersecting_campgrounds.empty:
+        # If there are wildfires inside the cell, color it red
+        fishnet_colors.append('green')
+    else:
+        # If there are no wildfires inside the cell, color it blue
+        fishnet_colors.append('lightgrey')
+
+# Add the 'color' column to the fishnet GeoDataFrame
+fishnet['color'] = fishnet_colors
+
+# Plotting
 fig, ax = plt.subplots(figsize=(10, 10))
+
+print("plotting now ...")
 
 # Plot California counties data
 california_data.plot(ax=ax, color='lightgrey', edgecolor='black', linewidth=0.5)
 
-# Plot filtered campgrounds that fall within California
-campgrounds_in_california.plot(ax=ax, color='green', alpha=0.7)
+# Plot filtered wildfires that fall within California
+campgrounds_in_california.plot(ax=ax, color='red', alpha=0.7)
 
-# Plot the clipped fishnet/grid overlay
-grid_clipped.boundary.plot(ax=ax, color='blue', linewidth=0.3)
+# Plot fishnet on top of the map, filling cells with red color
+fishnet.plot(ax=ax, facecolor=fishnet['color'], edgecolor='blue', linewidth=0.1)
 
-ax.set_title("Campgrounds Overlayed on California Counties with Fishnet Grid")
+ax.set_title("Campgrounds Overlayed on California Counties with Smaller Fishnet")
 plt.show()
--- kernalDensity.py
+++ kernalDensity.py
@@ -2,6 +2,14 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import gaussian_kde
+from shapely.geometry import Polygon
+
+import matplotlib.colors as mcolors
+
+def rgba_to_color_name(rgba_tuple):
+    color = mcolors.to_rgba(rgba_tuple)
+    color_name = mcolors.to_color(color)
+    return color_name
 
 california_data = gpd.read_file("./testData/CA_Counties_TIGER2016.geojson")
 wildfire_data = gpd.read_file("./testData/California_Fire_Perimeters_(all).geojson")
@@ -12,8 +20,9 @@ print(wildfire_data.crs)
 
 # Reproject wildfire_data to a projected CRS
 # Replace 'EPSG:4326' with the appropriate CRS code for your data if needed.
-wildfire_data = wildfire_data.to_crs('EPSG:3395')
+wildfire_data = wildfire_data.to_crs(california_data.crs)
 
+print("starting gaussian kernel density estimation ...")
 # Convert polygon/multipolygon geometries to their centroids (representative points)
 wildfire_data['centroid'] = wildfire_data.geometry.centroid
 
@@ -30,13 +39,65 @@ grid_coords = np.column_stack((x.ravel(), y.ravel()))
 
 # Evaluate the KDE on the grid points.
 density_values = kde(grid_coords.T).reshape(x.shape)
+# Calculate the bounding box for California counties
+bbox = california_data.total_bounds
+
+print("checking fishnet")
+
+# Create a fishnet grid with smaller cells covering the bounding box of California counties
+cell_size = 25000  # Cell size in meters (25 km = 25000 meters)
+rows = int((bbox[3] - bbox[1]) / cell_size)
+cols = int((bbox[2] - bbox[0]) / cell_size)
+
+# Generate polygons for each cell in the fishnet
+grid_polygons = []
+for x in range(cols):
+    for y in range(rows):
+        xmin = bbox[0] + x * cell_size
+        xmax = bbox[0] + (x + 1) * cell_size
+        ymin = bbox[1] + y * cell_size
+        ymax = bbox[1] + (y + 1) * cell_size
+
+        # Clip the fishnet cell to the extent of California
+        clipped_polygon = Polygon([(max(xmin, bbox[0]), max(ymin, bbox[1])),
+                                   (min(xmax, bbox[2]), max(ymin, bbox[1])),
+                                   (min(xmax, bbox[2]), min(ymax, bbox[3])),
+                                   (max(xmin, bbox[0]), min(ymax, bbox[3]))])
+        interscting_california = california_data[california_data.intersects(clipped_polygon)]
+        if not interscting_california.empty:
+            grid_polygons.append(clipped_polygon)
+
+fishnet = gpd.GeoDataFrame({'geometry': grid_polygons}, crs=california_data.crs)
+
+
+
+
+print("getting fishnet colors ...")
+fishnet_colors = []
+# Calculate the kernel density value at the centroid of each fishnet cell and store the corresponding color
+for cell_polygon in grid_polygons:
+    centroid_x, centroid_y = cell_polygon.centroid.x, cell_polygon.centroid.y
+    density_value = kde([centroid_x, centroid_y])
+    fishnet_colors.append(density_value)
+
+cmap = plt.get_cmap('inferno')
+norm = plt.Normalize(vmin=np.min(fishnet_colors), vmax=np.max(fishnet_colors))
+
+fishnet['color'] = fishnet_colors
+
+for cell_polygon in grid_polygons:
+    print(norm(cell_polygon['color']))
+    print(cmap(norm(cell_polygon['color'])))
 
 fig, ax = plt.subplots(figsize=(10, 10))
 
+print("plotting")
 # Plot California counties data on top of the heatmap.
 california_data.plot(ax=ax, color='lightgrey', edgecolor='black', linewidth=1.5, alpha=1, zorder=1)
-# Plot the heatmap using the density values on top of the California map.
-ax.imshow(density_values.T, origin='lower', extent=[xmin, xmax, ymin, ymax], cmap='inferno', alpha=0.8, zorder=2)
 
-ax.set_title("Kernel Density Estimation Heatmap of Wildfires in California")
+# Plot the heatmap using the density values on top of the California map.
+fishnet.boundary.plot(ax=ax, facecolor=rgba_to_color_name(cmap(norm(fishnet['color']))), edgecolor='black',linewidth=0.5)
+
+
+ax.set_title("Kernel Density Estimation Heatmap of Wildfires in California with Fishnet")
 plt.show()
