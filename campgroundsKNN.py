9e0d12a4a6290cb0ae245d240aa547f5cbc57750
Divya <divyamakkar000@gmail.com>
2023-08-03 14:56:37 -0400

    camp KNN

--- campgroundsKNN.py
+++ campgroundsKNN.py
@@ -1,6 +1,15 @@
 import geopandas as gpd
 import matplotlib.pyplot as plt
 from shapely.geometry import Polygon
+import numpy as np
+from scipy.stats import gaussian_kde
+from shapely.geometry import Polygon
+
+# I want a kNN algorithm that will take each fishnet cell and find the nearest wildfire to it
+
+# fundnction to find distance between two points
+def distanceBetween (point1, point2):
+    return ((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)**0.5
 
 california_data = gpd.read_file("./testData/CA_Counties_TIGER2016.geojson")
 campground_data = gpd.read_file("./testData/Campground Sites.geojson")
@@ -20,13 +29,15 @@ campgrounds_in_california = gpd.sjoin(campground_data_reprojected, california_da
 
 wildfire_data_reprojected = wildfire_data.to_crs(california_data.crs)
 
+wildfires_in_california = gpd.sjoin(wildfire_data_reprojected, california_data, op="within")
+
 # Calculate the bounding box for California counties
 bbox = california_data.total_bounds
 print(bbox)
 
 print("fishnet stage 1 loading ...")
 # Create a fishnet grid with smaller cells covering the bounding box of California counties
-cell_size = 5000  # Cell size in meters (25 km = 25000 meters)
+cell_size = 10000  # Cell size in meters (25 km = 25000 meters)
 rows = int((bbox[3] - bbox[1]) / cell_size)
 cols = int((bbox[2] - bbox[0]) / cell_size)
 
@@ -53,7 +64,7 @@ print("checking fishnet ...")
 fishnet = gpd.GeoDataFrame({'geometry': grid_polygons}, crs=california_data.crs)
 
 # Create a list to store the color for each fishnet cell
-fishnet_colors = []
+
 
 wildfire_coordinates = []
 
@@ -65,17 +76,24 @@ for cell_polygon in fishnet['geometry']:
 
     if not intersecting_wildfires.empty:
         # If there are wildfires inside the cell, color it red
-        wildfire_coordinates.append([cell_polygon.centroid_x, cell_polygon.centroid_y])
+        wildfire_coordinates.append([cell_polygon.centroid.x, cell_polygon.centroid.y])
 
-    if not intersecting_campgrounds.empty:
-        # If there are wildfires inside the cell, color it red
-        fishnet_colors.append('green')
-    else:
-        # If there are no wildfires inside the cell, color it blue
-        fishnet_colors.append('lightgrey')
 
-# Add the 'color' column to the fishnet GeoDataFrame
-fishnet['color'] = fishnet_colors
+fishnet_distance = []
+
+for cell_polygon in grid_polygons:
+    distance = 10000000000000
+    for wildfire in wildfire_coordinates:
+        distance = min(distance, distanceBetween([cell_polygon.centroid.x, cell_polygon.centroid.y], wildfire))
+    fishnet_distance.append(distance)
+    print(distance)
+
+fishnet['distance'] = fishnet_distance
+
+
+cmap = plt.get_cmap('inferno_r')
+norm = plt.Normalize(vmin=np.min(fishnet_distance), vmax=np.max(fishnet_distance))
+
 
 # Plotting
 fig, ax = plt.subplots(figsize=(10, 10))
@@ -89,7 +107,12 @@ california_data.plot(ax=ax, color='lightgrey', edgecolor='black', linewidth=0.5)
 campgrounds_in_california.plot(ax=ax, color='red', alpha=0.7)
 
 # Plot fishnet on top of the map, filling cells with red color
-fishnet.plot(ax=ax, facecolor=fishnet['color'], edgecolor='blue', linewidth=0.1)
+fishnet.boundary.plot(ax=ax, facecolor=cmap(norm(fishnet['distance'])), edgecolor='black', linewidth=0.5)
 
 ax.set_title("Campgrounds Overlayed on California Counties with Smaller Fishnet")
 plt.show()
+
+
+
+
+
