b8ac896b23adb11ffe7075878477826bb8e6a27e
Blueishfiend692 <aditymakkar000@gmail.com>
2023-08-04 15:38:19 -0400

    new clone

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ print(bbox)
 
 print("fishnet stage 1 loading ...")
 # Create a fishnet grid with smaller cells covering the bounding box of California counties
-cell_size = 5000  # Cell size in meters (25 km = 25000 meters)
+cell_size = 25000  # Cell size in meters (25 km = 25000 meters)
 rows = int((bbox[3] - bbox[1]) / cell_size)
 cols = int((bbox[2] - bbox[0]) / cell_size)
 
